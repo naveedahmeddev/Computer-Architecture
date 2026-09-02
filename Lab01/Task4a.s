6c71e49a34e28e175dce80af56e0089211deb9c1
naveedahmeddev <naveedahmedofficial2@gmail.com>
2026-09-02 18:58:02 +0500

    tasks 3 and 4a

---
+++ Lab01/Task4a.s
@@ -0,0 +1,18 @@
+.text 
+main:
+    li x10, 0x78786464 
+    li x11, 0xA8A81919
+
+    sw x10, 0x100(x0)   # Store x10 as unsigned integer at address 0x100.
+
+    sw x11, 0x1F0(x0)  
+
+    lhu x11, 0x100(x0)
+
+    lh x13, 0x1F0(x0)
+
+    lb x14, 0x1F0(x0)
+
+end:
+    j end
+
