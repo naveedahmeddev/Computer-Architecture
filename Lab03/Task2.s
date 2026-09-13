73565d22e9907562faad7aed7f4b588a69f75354
naveedahmeddev <naveedahmedofficial2@gmail.com>
2026-09-13 12:51:25 +0500

    Task 04 remaining

--- Lab03/Task2.s
+++ Lab03/Task2.s
@@ -2,19 +2,20 @@
 
 main:
     addi sp, sp, -32
-    sw x10, 24(sp)
-    sw x11, 16(sp)
+    sw x10, 24(sp)     # store the parameter registers and x1 in the stack
+    sw x11, 16(sp)    
     sw x12, 8(sp)
     sw x13, 0(sp)
 
     jal x1, leaf_example
-    addi x11, x10, 0
-    li x10, 1
+    addi x11, x10, 0      # store the return value in x11 
+    addi sp, sp, 32        # restore the stack pointer
+    li x10, 1      
     ecall
     j exit 
 
 leaf_example:
-    addi sp, sp, -24
+    addi sp, sp, -24    # store the saved registers in the stack
     sw, x18, 16(sp)
     sw, x19, 8(sp)
     sw, x20, 0(sp)
@@ -24,17 +25,17 @@ leaf_example:
     li x12, 5    # i 
     li x13, 6    # j 
 
-    li x20, 0
-    add x18, x10, x11 
-    add x19, x12, x13
+    li x20, 0         # f = 0
+    add x18, x10, x11   # g + h
+    add x19, x12, x13  # i + j
 
-    sub x20, x18, x19 
-    addi x10, x20, 0
+    sub x20, x18, x19 # f = g + h - (i + j)
+    addi x10, x20, 0   # store the return value in x10
 
     lw, x18, 16(sp)
     lw, x19, 8(sp)
     lw, x20, 0(sp)
-
+    addi sp, sp, 24   # restore the stack pointer
     jalr x0, 0(x1)
 
 
--- Lab03/Task3.s
+++ Lab03/Task3.s
@@ -2,12 +2,15 @@
 
 main: 
     # assume x10 = base address, x11 = index of the element which gets swapped
-    add sp, sp, -8   # pushing to stack the contents of the argument registers
+    addi sp, sp, -8   # pushing to stack the contents of the argument registers
     sw x10, 4(sp)
     sw x11, 0(sp)
 
     jal x1, swap 
-
+    lw x10, 4(sp)
+    lw x11, 0(sp)
+    addi sp, sp, 8   # restore the stack pointer
+    beq x0, x0, Exit
 
 swap: 
     
@@ -15,10 +18,12 @@ swap:
     addi x29, x11, 0 # x29 = k 
     li x30, 0
     slli, x30, x29, 2
-    add x31, x30 + x10   # address of kth element 
-    addi x5, x31, 4 
-    lw x6, 0(x31) 
-    lw x7, 0(x5) 
-    sw x7, 0(x31)
-    
+    add x31, x30, x10   # address of kth element 
+    addi x5, x31, 4   # address of (k+1)th element
+    lw x6, 0(x31)   # temp = D[k]
+    lw x7, 0(x5)  # D[k+1]
+    sw x7, 0(x31)   # D[k] = D[k+1]
+    sw x6, 0(x5)    # D[k+1] = temp 
+    jalr x0, 0(x1)  
 
+Exit: 
