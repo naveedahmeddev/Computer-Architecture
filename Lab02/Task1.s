ec5a492cafabd9c5d3611fe55fb77afc44dde0af
naveedahmeddev <naveedahmedofficial2@gmail.com>
2026-09-13 12:10:05 +0500

    Lab 02 Tasks completed

--- Lab02/Task1.s
+++ Lab02/Task1.s
@@ -1,20 +1,20 @@
-.text
+.text 
+main:
 
-main: 
-    # assuming that variable f to j are in registers x19 - x23 
-    li x22, 3
+# Listing 3 
+# assuming that variables f to j are in registers x19-x23
     bne x22, x23, Else
-    add x19, x20, x21 
-    beq x0, x0, Exit # unconditional jump 
+    add x19, x20, x21
+    beq x0, x0, Exit1     # unconditional jump
 Else: sub x19, x20, x21
+Exit1: 
 
-
-Loop: slli x10, x22, 2  #Temp register x10 = i * 4
-    add x10, x10, x25
-    lw x9, 0(x10)
-    bne x9, x24, Exit
-    addi x22, x22, 1  
-    beq x0, x0, Loop 
-
-
-Exit: 
+# Listing 4 
+# assuming i and k in x22 and x24, and the base address of Save in x25
+Loop: slli x10, x22, 2    # Temp reg x10 = i * 4
+    add x10, x10, x25     # x10 = address of save[i]
+    lw x9, 0(x10)         # Temp reg x9 = save[i]
+    bne x9, x24, Exit2    # go to Exit2 if save[i] != k
+    addi x22, x22, 1      # i = i + 1
+    beq x0, x0, Loop      # go to Loop
+Exit2: 
--- Lab02/Task4.s
+++ Lab02/Task4.s
@@ -1,24 +1,22 @@
 .text
 main:
-    # a = x5, b = x6 , i = x7, j = x29
-    li x5, 2
-    li x6, 2 
-    li x7, 0
-    li x29, 0
-Outerloop: 
-    bge x7, x5, Exit
-    beq x0, x0, InnerLoop
-    InnerLoop: 
-        bge x29, x6, Increment_i
-        slli x29, x29, 2   # 4*j 
-        add x28, x7, x29  # i+ j 
-        sw x28 , 0(x29)
-        addi x29, x29, 1   # j ++ 
-        beq x0,x0, InnerLoop
-    
-Increment_i: 
-    li x29, 0
-    addi x7, x7, 1   # i++ 
-    beq x0, x0, Outerloop
-
+    # a = x5, b = x6, i = x7, j = x29, x10 = base of D
+    li x10, 0x10000000        # base address of D
+    li x5, 3               # a = 3
+    li x6, 4               # b = 4
+    li   x7, 0               # i = 0
+OuterLoop:
+    bge  x7, x5, Exit        # i >= a -> done
+    li   x29, 0              # j = 0
+InnerLoop:
+    bge  x29, x6, Increment_I       # j >= b -> next i
+    slli x28, x29, 4         # x28 = 16*j  (index 4*j, times 4 bytes/element)
+    add  x28, x10, x28       # x28 = address of D[4*j]
+    add  x30, x7, x29        # x30 = i + j
+    sw   x30, 0(x28)         # D[4*j] = i + j
+    addi x29, x29, 1         # j++
+    beq  x0, x0, InnerLoop
+Increment_I:
+    addi x7, x7, 1           # i++
+    beq  x0, x0, OuterLoop
 Exit:
